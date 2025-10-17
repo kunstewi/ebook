8c3a6b23bd1aa1309ad410cd7f39adc7f9a0c81c
kunstewi <kan.ai35423@gmail.com>
2025-10-17 10:28:06 +0530

    added server.js

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+// Express Instance
+const app = express();
+
+// MIddleware to handle CORS
+app.use(
+  cors({
+    origin: "*",
+    method: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middleware to Parse JSON
+app.use(express.json());
+
+// Static folder for uploads
+app.use("backend/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
