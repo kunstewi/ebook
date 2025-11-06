4f8e89e461abe63100e560c3af4cf7eb7dc1e6e6
kunstewi <kan.ai35423@gmail.com>
2025-11-06 17:56:39 +0530

    init frontend file structure

---
+++ frontend/src/components/layout/ProfileDropdownLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileDropdownLayout = () => {
+  return (
+    <div>ProfileDropdownLayout</div>
+  )
+}
+
+export default ProfileDropdownLayout
---
+++ frontend/src/components/view/ViewChapterSideline.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewChapterSideline = () => {
+  return (
+    <div>ViewChapterSideline</div>
+  )
+}
+
+export default ViewChapterSideline
