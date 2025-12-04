09b080dd038f739245aee2f9c2a2f8bd7470d8e8
kunstewi <kan.ai35423@gmail.com>
2025-12-04 11:49:45 +0530

    added home register and login page

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,10 +1,27 @@
 const Book = require("../models/Book");
 
-// @desc    Create a new book
-// @route   POST /api/books
-// @access  Private
+// @desc      Create a new book
+// @route     POST /api/books
+// @access    Private
 const createBook = async (req, res) => {
   try {
+    const { title, author, subtitle, chapters } = req.body;
+
+    if (!title || !author) {
+      return res
+        .status(400)
+        .json({ message: "Please provide a title and author" });
+    }
+
+    const book = await Book.create({
+      userId: req.user._id,
+      title,
+      author,
+      subtitle,
+      chapters,
+    });
+
+    res.status(201).json(book);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -15,6 +32,10 @@ const createBook = async (req, res) => {
 // @access  Private
 const getBooks = async (req, res) => {
   try {
+    const books = await Book.find({ userId: req.user._id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(books);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -25,6 +46,19 @@ const getBooks = async (req, res) => {
 // @access  Private
 const getBookById = async (req, res) => {
   try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.userId.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to view this book" });
+    }
+
+    res.status(200).json(book);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -35,6 +69,23 @@ const getBookById = async (req, res) => {
 // @access  Private
 const updateBook = async (req, res) => {
   try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.userId.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to update this book" });
+    }
+
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(updatedBook);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -45,6 +96,21 @@ const updateBook = async (req, res) => {
 // @access  Private
 const deleteBook = async (req, res) => {
   try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.userId.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to delete this book" });
+    }
+
+    await book.deleteOne();
+
+    res.status(200).json({ message: "Book deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -55,10 +121,32 @@ const deleteBook = async (req, res) => {
 // @access  Private
 const updateBookCover = async (req, res) => {
   try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.userId.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to update this book" });
+    }
+
+    if (req.file) {
+      book.coverImage = `/${req.file.path}`;
+    } else {
+      return res.status(400).json({ message: "No image file provided" });
+    }
+
+    const updatedBook = await book.save();
+
+    res.status(200).json(updatedBook);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 };
+
 
 module.exports = {
   createBook,
